80a9926f9979aca4d6f791abfc1a532e07515e08
DawePhil01 <96893185+DawePhil01@users.noreply.github.com>
2023-10-28 18:10:52 -0400

    Covid19 pandemic.sql

--- covid19 pandemic.sql
+++ covid19 pandemic.sql
@@ -2,24 +2,25 @@ USE COVID_19;
 
 /*
 
-For this Project, I am analysing the impact of covid 19 in Canada and USA
-I got the data from ourworldindata.org. It had data about covid 19 for different countries around the world
-but i extracted only the data from Canada and USA, so i can analyze it
+For this Project, I am analyzing the impact of covid 19 in Canada and the USA
+I got the data from ourworldindata.org. It had data about covid 19 from different countries around the world
+but I extracted only the data from Canada and the USA and transformed it.
+I also cleaned the data by removing duplicates, and blank rows, so I could analyze it.
 
 */
 
 
--- First table is the covid deaths
+-- The first table is the COVID-19 deaths
 SELECT *
 FROM covid_deaths_canada_usa;
 
 
--- Second table is the covid vaccinations
+-- Second table is the COVID vaccinations
 SELECT * 
 FROM covid_vaccine_canada_usa;
 
 -- Looking at the Total Covid-19 Cases and Total Deaths
--- Comparing to see the percentage of people who died after getting covid in Canada and USA
+-- Comparing the percentage of people who died after getting COVID in Canada and the USA
 
 
 Select Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as Death_Percentage
@@ -27,20 +28,20 @@ FROM covid_deaths_canada_usa
 order by location, date;
 
 
--- Looking at the Total covid 19 cases vs Population
--- I want to see what’s the percentage of the population got the covid 19 each
+-- Looking at the Total COVID-19 cases vs the Population
+-- I want to see what is the percentage of the population got the covid 19 each
 Select location, date, population,total_cases, (total_cases/population)*100 as Percentage_People_infected
 FROM covid_deaths_canada_usa
 order by location, date;
 
--- Looking which Country between Canada and USA had the highest infection rate compared to the Population
+-- Looking which Country between Canada and the USA had the highest infection rate compared to the Population
 
 Select Location, population, Max(total_cases) as total_Covid_19_Cases,MAX((total_cases/population)*100 )AS Percentage_People_infected
 FROM covid_deaths_canada_usa
 Group by location, population;
 
 
--- 	Looking at which Country had with Highest Death Counts per Population
+-- 	Looking at which Country had the Highest Death Counts per Population
 
 Select Location, max(total_deaths) as Total_Deaths
 FROM covid_deaths_canada_usa
@@ -49,8 +50,8 @@ Group by location;
 
 -- Looking at the numbers
 
--- Here we are looking from the beginning, for each day, how many people caught covid,
--- how many people died and the death percentage around Canada and USA
+-- Here we are looking from the beginning, for each day, how many people caught COVID-19,
+-- How many people died and the death percentage around Canada and the USA
 
 
 select date, sum(new_cases) as total_case, sum(new_deaths) as total_deaths, sum(new_deaths)/sum(new_cases) as DeathPercentage
@@ -58,7 +59,7 @@ from covid_deaths_canada_usa
 group by date
 Order by date;
 
--- This is to look at how many people caught the covid 19 and how many died from it and the death Percentage
+-- This is to look at how many people caught COVID-19, and how many died from it and the death Percentage
 
 select sum(new_cases) as total_case, sum(new_deaths) as total_deaths, sum(new_deaths)/sum(new_cases) as DeathPercentage
 from covid_deaths_canada_usa;
